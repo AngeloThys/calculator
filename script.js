21676578e026fe5d7f9b8ced2c952cbbbf4f4adb
Angelo Thys <thys.angelo@gmail.com>
2023-08-23 12:37:30 +0100

    Add clear functionality, refactor number event listeners

--- script.js
+++ script.js
@@ -38,26 +38,21 @@ function display(number) {
 }
 
 const calculatorDisplay = document.querySelector(".display");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const zero = document.querySelector(".zero");
-const point = document.querySelector(".point");
 
-one.addEventListener('click', display.bind(null, 1));
-two.addEventListener('click', display.bind(null, 2));
-three.addEventListener('click', display.bind(null, 3));
-four.addEventListener('click', display.bind(null, 4));
-five.addEventListener('click', display.bind(null, 5));
-six.addEventListener('click', display.bind(null, 6));
-seven.addEventListener('click', display.bind(null, 7));
-eight.addEventListener('click', display.bind(null, 8));
-nine.addEventListener('click', display.bind(null, 9));
-zero.addEventListener('click', display.bind(null, 0));
-point.addEventListener('click', display.bind(null, '.'));
+const numberButtons = document.querySelectorAll(".button-numbers > button");
+
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => display(numberButton.textContent));
+});
+
+function clear() {
+    numberOne = null;
+    operator = null;
+    numberTwo = null;
+
+    calculatorDisplay.textContent = '';
+}
+
+const clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener('click', clear);
